6dd1f8b44c3575ff60172a5666602dd303179087
gweesip <aj.marasco88@gmail.com>
2022-06-23 16:57:00 -0400

    Updated project directory to fix install/import issues

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from extendableenum import inheritable_enum, \
-    set_auto_null, auto_null_member, AutoNullEnum, \
-    post_mixin_enum, \
-    copy_enum_members
-
-__all__ = ['inheritable_enum',
-           'set_auto_null', 'auto_null_member', 'AutoNullEnum',
-           'post_mixin_enum',
-           'copy_enum_members']
